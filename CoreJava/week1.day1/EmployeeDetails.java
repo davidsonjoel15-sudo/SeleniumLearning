1db5c66c3292f1bf695cec56e262b76dd9dc6c61
davidsonjoel15-sudo <davidsonjoel15@gmail.com>
2025-08-20 16:36:36 +0530

    Add files via upload

---
+++ CoreJava/week1.day1/EmployeeDetails.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class EmployeeDetails {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub 		
+			String name = "John Doe";
+			byte age = 30;
+			float salary = 55000.75f;
+			char gender = 'M';
+			boolean acceptTerms = true;
+			float rating = 4.5f;
+			String mobileNumber = "9876543210";
+			int pinCode = 56001;
+			int luckyNumber = 7;	
+			System.out.println("Employee Name:" +name);
+			System.out.println("Employee Age:" +age);
+			System.out.println("Employee Salary:" +salary);
+			System.out.println("Employee Gender:" +gender);
+			System.out.println("Employee AcceptTerms:" +acceptTerms);
+			System.out.println("Employee Rating:" +rating);
+			System.out.println("Employee Mobile Number:" +mobileNumber);
+			System.out.println("Employee Pin Code:" +pinCode);
+			System.out.println("Employee Lucky Number:" +luckyNumber);
+		}
+	}
+
